3ae3fa5c2b76537cc5e818406577bc03593262cc
Xtail <xtail1996@yandex.ru>
2017-05-29 04:23:30 +0300

    Добавлен вывод промежуточных данных

--- course.py
+++ course.py
@@ -1,39 +1,69 @@
 from graphviz import Graph, Digraph
+import os.path
+inf = float('inf')
 
-def printGraph(graph, graphType):
-	if graphType == "directed":
-		g = Digraph()
-	else:
-		g = Graph()
+def bellman_ford(graph, source, graphType):
+    img_counter = 0
+    def printGraph(graph, graphType, d, node, neighbour, description, outdir = 'out'):
+        nonlocal img_counter
+        if graphType == "directed":
+            g = Digraph(format = "png")
+        else:
+            g = Graph(format = "png")
 
-	for u in graph:
-		for v in graph[u]: 
-			g.node(u, u)
-			g.node(v, v)
-			g.edge(u, v, label = str(graph[u][v]))
-	g.view()
+        font = "Arial"
+        g.attr('graph', fontname=font)
+        g.attr('node', fontname=font)
+        g.attr('edge', fontname=font)
+        g.attr(rankdir = "LR")
+
+        for u in graph:
+            for v in graph[u]: 
+                color = "black"
+                g.node(u, u)
+                e = graph[u][v]
+
+                # source -> node -> neighbor - green
+                # source -> neighbour - red
+                if(u == source and v == node) or (u == node and v == neighbour):
+                    color = "green"
+                elif u == source and v == neighbour:
+                    color = "red"
+                g.edge(u, v, label = str(e), color = color)
+
+        if (source != node):
+            g.edge(source, node, label = str(d[node]), style = "dashed", color = "green")
+        #g.view()
+        g.attr(label=description)
+        g.render(filename=os.path.join(os.path.abspath(outdir), str(img_counter)), cleanup=True)
+        img_counter += 1
+
+    # Step 1: For each node prepare the destination and predecessor
+    def initialize(graph, source):
+        d = {} # Stands for destination
+        p = {} # Stands for predecessor
+        for node in graph:
+            d[node] = float('Inf') # We start admiting that the rest of nodes are very very far
+            p[node] = None
+        d[source] = 0 # For the source we know how to reach
+        return d, p
+
+    def relax(node, neighbour, graph, d, p):
+        # If the distance between the node and the neighbour is lower than the one I have now
+        if d[neighbour] > d[node] + graph[node][neighbour]:
+            hint = "Релаксация прошла успешно"
+            printGraph(graph, graphType, d, node, neighbour, hint)
+            # Record this lower distance
+            d[neighbour]  = d[node] + graph[node][neighbour]
+            p[neighbour] = node
+            
 
 
-
-# Step 1: For each node prepare the destination and predecessor
-def initialize(graph, source):
-    d = {} # Stands for destination
-    p = {} # Stands for predecessor
-    for node in graph:
-        d[node] = float('Inf') # We start admiting that the rest of nodes are very very far
-        p[node] = None
-    d[source] = 0 # For the source we know how to reach
-    return d, p
-
-def relax(node, neighbour, graph, d, p):
-    # If the distance between the node and the neighbour is lower than the one I have now
-    if d[neighbour] > d[node] + graph[node][neighbour]:
-        # Record this lower distance
-        d[neighbour]  = d[node] + graph[node][neighbour]
-        p[neighbour] = node
-
-def bellman_ford(graph, source):
     d, p = initialize(graph, source)
+    hint = "Задан граф, необходимо найти путь из вершины {source} в остальные вершины графа".format(source = source)
+    printGraph(graph, graphType, d, 'a', 'a', hint)
+
+
     for i in range(len(graph)-1): #Run this until is converges
         for u in graph:
             for v in graph[u]: #For each neighbour of u
@@ -48,20 +78,20 @@ def bellman_ford(graph, source):
 
 
 def read_data():
-	inputData = []
-	try:
-		f = open("input.txt", "r")
-		graphType = f.readline()
-		graphType = graphType.rstrip("\n")
-		for line in f:
-			if line :
-				string_list = line.split()
-				inputData.append( [string_list[0], string_list[1], int(string_list[2])] )
-		f.close()
-	except IOError:
-		print("Can not open input file")
+    inputData = []
+    try:
+        f = open("input.txt", "r")
+        graphType = f.readline()
+        graphType = graphType.rstrip("\n")
+        for line in f:
+            if line :
+                string_list = line.split()
+                inputData.append( [string_list[0], string_list[1], int(string_list[2])] )
+        f.close()
+    except IOError:
+        print("Can not open input file")
 
-	return graphType, inputData
+    return graphType, inputData
 
 
 
@@ -75,18 +105,18 @@ print(graphType)
 
 graph = {}
 for src, dest, length in data:
-	if src not in graph:
-		graph.update({ src : {} })
-	if dest not in graph:
-		graph.update({ dest : {} })
-	graph[src].update({dest : length})
-	
+    if src not in graph:
+        graph[src] = {}
+    if dest not in graph:
+        graph[dest] = {}
+    graph[src][dest] = length
+
 print(graph)
 
-d, p = bellman_ford(graph, 'a')
+d, p = bellman_ford(graph, 'a', graphType)
 
 print(d)
 print(p)
 
-printGraph(graph, graphType)
+#printGraph(graph, graphType)
 
